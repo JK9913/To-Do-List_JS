340912d6a1893612bfaa4f7d6413fb12046e960c
Jan-Kristian Karlsen <jan-kristian@Jan-Kristians-MacBook-Air.local>
2024-08-14 09:28:16 +0200

    Created a seperate file for creating task. The class system is set up in this file and will return a finished task

---
+++ src/create_task.js
@@ -0,0 +1,78 @@
+import _ from 'lodash';
+import './style.css';
+import {format, parseISO, addDays, isValid} from 'date-fns';
+
+export default function createTask() {
+  // Write a class that creates to-do tasks and belong to the project variable
+  class Task {
+    constructor(title = "Placeholder title", description = "Please enter a description", dueDate = addDays(new Date(), 1), priority = 0, notes = "") {
+      this.title = title;
+      this.description = description;
+      this.dueDate = dueDate;
+      this.priority = priority;
+      this.notes = notes;
+    }
+
+    get viewDueDate() {
+      return format(this.dueDate, "yyyy-MM-dd");
+    }
+
+    set editDueDate(date) {
+      if (date === "") {
+        console.log("Date cannot be empty");
+        return;
+      } else if (date.length !== 10 && date[2] !== "." && date[5] !== ".") {
+        console.log("Date must be in the format dd.mm.yyyy");
+        return;
+      }
+
+      const parsedDate = parseISO(date.split('.').reverse().join('-')); // Convert dd.mm.yyyy to yyyy-mm-dd
+      if (!isValid(parsedDate)) {
+      console.log("Invalid date");
+      return;
+     }
+
+      this.dueDate = parsedDate;
+    }
+
+    get viewTitle() {
+      return this.title;
+    }
+
+    set editTitle(title) {
+      if (title === "") {
+        console.log("Title cannot be empty");
+        return;
+      } else if (title.length > 50) {
+        console.log("Title must be less than 50 characters");
+        return;
+      }
+
+      this.title = title;
+    }
+
+    get viewDescription() {
+      return this.description;
+    }
+
+    set editDescription(description) {
+      this.description = description;
+    }
+
+    set editProirity(priority) {
+      if (priority < 0 || priority > 3) {
+        console.log("Priority must be between 0 and 3");
+        return;
+      }
+      
+      this.priority = priority;
+    }
+
+    get viewPriority() {
+      return this.priority;
+    }
+
+  }
+
+  return Task;
+} 
